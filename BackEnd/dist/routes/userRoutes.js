ca29404065b73c5de54bc36aba64bb9713230355
Stephen Ondieki <stevehotcodes@gmail.com>
2023-11-05 22:19:49 +0300

    added the route for decoding data from token

--- BackEnd/dist/routes/userRoutes.js
+++ BackEnd/dist/routes/userRoutes.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const userControllers_1 = require("../controllers/userControllers");
+const verifyToken_1 = require("../middlewares/verifyToken");
 const userRouter = (0, express_1.Router)();
+userRouter.get("/checkuserdetails", verifyToken_1.verifyToken, userControllers_1.checkUserDetails);
 userRouter.post('/new', userControllers_1.registerNewUser);
 userRouter.get('/unassigned', userControllers_1.getUnassignedUsers);
 userRouter.get('/all', userControllers_1.getAllUser);
